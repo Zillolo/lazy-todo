ceb5c07db0aceb0afb38b9f3380a4d5b66fd8b71
Alex Egger <alex.egger96@gmail.com>
2015-09-22 21:28:40 +0200

    Add addTask function and TaskError class

--- app/task.py
+++ app/task.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, DateTimeField, EmailField, IntField,  \
     ReferenceField, StringField, ValidationError
-import datetime, enum, Exception
+import datetime, enum
 
 from app import logger
 
@@ -33,7 +33,7 @@ class Task(Document):
         status (Status): The current status of the Task.
         priority(Priority): The priority level of the Task.
     """
-    title = StringField(max_length=150, required=True)
+    title = StringField(max_length=150, unique=True, required=True)
     description = StringField(max_length=800, required=True)
 
     creator = EmailField(max_length=120, required=True)
@@ -43,3 +43,40 @@ class Task(Document):
 
     status = IntField(default=Status.OPEN, required=True)
     priority = IntField(default=Priority.LOW, required=True)
+
+class TaskError(Exception):
+    """
+    An exception to be used for all error that can occure during handling of tasks.
+    """
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+def addTask(title, description, creator, assigne, created_at=None, status=None,
+    priority=None):
+    """
+    Adds a new task to the list.
+    """
+    task = Task()
+    task.title = title
+    task.description = description
+    task.creator = creator
+    task.assigne = assigne
+
+    if created_at is not None:
+        task.created_at = created_at
+    if status is not None:
+        task.status = status
+    if priority is not None:
+        task.priority = priority
+
+    try:
+        logger.debug('A new task will be inserted. (Title: {0})'.format(
+            task.title))
+
+        task.save()
+    except ValidationError as e:
+        logger.exception('An exception has been encountered during the insertion of'
+            'a task.')
+        raise TaskError('Couldn\'t add your task to the list.')

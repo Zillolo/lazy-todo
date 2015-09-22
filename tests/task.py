d8da4aec560a4f05114c0564400bd5ab0592d5d9
Alex Egger <alex.egger96@gmail.com>
2015-09-22 21:29:06 +0200

    Add tests for addTask

--- tests/task.py
+++ tests/task.py
@@ -1,7 +1,8 @@
 from mongoengine import ValidationError
 from nose.tools import raises
+import datetime
 
-from app.task import Priority, Task
+from app.task import Priority, Status, Task, TaskError, addTask
 
 def test_Set_Title_String():
     task = Task()
@@ -24,3 +25,19 @@ def test_Set_Priority_High():
 
     task.priority = Priority.HIGH
     assert task.priority == Priority.HIGH
+
+def test_Add_Task_Full():
+    addTask('Title', 'Description', 'email@email.com', 'email@email.com',
+        status = Status.CLOSED, created_at = datetime.datetime.now,
+        priority = Priority.MIDDLE)
+
+    # Retrieve the just inserted document
+    task = Task.objects(title = 'Title').first()
+    assert Task is not None
+
+    # Remove the inserted task
+    task.delete()
+
+@raises(TaskError)
+def test_Add_Task_Title_Int():
+    addTask(1, '', 'email@email.com', 'email@email.com')

e19dd09b0243c2edd5c3d2d18399f5a11b9c892b
Alex Egger <alex.egger96@gmail.com>
2015-09-22 21:02:08 +0200

    Add a logging instance to the package

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-import mongoengine
+import logging, mongoengine
 
 # Open a connection to the MongoDB database, to be shared throughout the
 #   application.
 db = mongoengine.connect('lazy', host='127.0.0.1', port=27017)
+
+# Create an instance of a logger
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
